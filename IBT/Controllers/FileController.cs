3dac7dda62a3ea5c140a3b6d8245f3c05831b551
Pavel Koryakin <pkoryakin@devteam.powerhub.com>
2018-09-01 01:56:55 -0400

    minor fixes

---
+++ IBT/Controllers/FileController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System.Linq;
+
+namespace IBT.Controllers
+{
+    public class FileController : Controller
+    {
+        private readonly IStringLocalizer _localizer;
+
+        public FileController(IStringLocalizerFactory factory)
+        {
+            var type = typeof(SharedResource);
+            _localizer = factory.Create(type);
+        }
+
+        public FileResult Index(string file)
+        {
+            var filename = _localizer.GetString(file);
+            var stream = Resources.Files.ResourceManager.GetMemoryStream(file);
+            return File(stream, "application/msword", $"{filename}.docx");
+        }
+
+        public FileResult Abu(int id)
+        {
+            if(!new int[] { 5,10,25 }.Any(q => q == id))
+            {
+                id = 25;
+            }
+
+            var abu = _localizer.GetString("Abu", id);
+            var filename = _localizer.GetString("abuSpecsFilename");
+            var stream = Resources.Files.ResourceManager.GetMemoryStream($"abu{id}specs");
+            return File(stream, "application/msword", $"{abu} {filename}.docx");
+        }
+    }
+}
---
+++ IBT/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Threading.Tasks;
+
+namespace IBT
+{
+    public static class Extensions
+    {
+        public static MemoryStream GetMemoryStream(this ResourceManager resourceManager, String name)
+        {
+            object resource = resourceManager.GetObject(name);
+
+            if (resource is byte[])
+            {
+                return new MemoryStream((byte[])resource);
+            }
+            else
+            {
+                throw new System.InvalidCastException("The specified resource is not a binary resource.");
+            }
+        }
+    }
+}
